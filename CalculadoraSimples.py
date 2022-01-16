af845fa100d3eddc5bae4423f6c2317f406f63b3
xwrqx <williamrs.12@gmail.com>
2022-01-15 23:44:39 -0300

    add code.py

---
+++ CalculadoraSimples.py
@@ -0,0 +1,29 @@
+while True:
+    num_1 = input('Digite um número: \n')
+    num_2 = input('Digite outro número: \n')
+    operador = input('Digite um operador: \n')
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print(f'{num_1} ou {num_2} não são válidos. Você precisa digitar um número inteiro.')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    else:
+        print(f'Operador {operador} inválido, digite outro operador (+, -, /, * )')
+
+    questionContinue = input('Deseja continuar? \n')
+    questionContinue = questionContinue.lower()
+    if questionContinue == 'y':
+        continue
+    else:
+        break
